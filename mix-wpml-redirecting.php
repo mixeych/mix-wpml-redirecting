7234334796729af1caad0cf1f97843e9240c50bc
mixeych <dmitriy_miheev@mail.ru>
2016-05-25 17:37:48 +0300

    finaly

--- mix-wpml-redirecting.php
+++ mix-wpml-redirecting.php
@@ -9,28 +9,52 @@ Author URI: http://itb-inc.net
 
 define('MIX_WPML_PLUGIN_PATH', dirname(__FILE__));
 
-require_once 'IP2Location-PHP-Module-master/IP2Location.php';
+global $mixWpmlCountryFlags;
+$mixWpmlCountryFlags = require_once( MIX_WPML_PLUGIN_PATH.'/locale/flags.php');
 
-add_action('init', 'MIXWPMLgeoRedirect');
-function MIXWPMLgeoRedirect(){
+add_action('init', 'geoRedirect');
+
+function geoRedirect(){
+	if(!class_exists('SitePress')){
+		return;
+	}
+	global $sitepress;
+	if($_SERVER['REQUEST_URI'] === '/en/'||$_SERVER['REQUEST_URI'] === '/en'){
+
+		$redirect = $sitepress->language_url( 'en' );
+		wp_redirect($redirect);
+		die();
+
+	}
+	
 	$sessStatus = session_status();
 	if($sessStatus !== 'PHP_SESSION_ACTIVE'&& $sessStatus !== 2){
 		session_start();
 	}
+
 	if($_SESSION['mccvis']){
 		return;
 	}
+
 	$_SESSION['mccvis'] = 1;
-	global $sitepress;
+	
 	$langs = icl_get_languages('skip_missing=N&orderby=KEY&order=DIR&link_empty_to=str');
 	$current_language = $sitepress->get_current_language();
 
 	$records = MIXGetCountryByUserIp();
+
 	if($records['countryCode']){
+
+		//remove after
+		if($records['countryCode'] == 'IL'){
+			return;
+		}
+		/**/
 		$userCountry = getLangCodeByCountry($records['countryCode']);
 		if(!$userCountry){
 			return;
 		}
+		
 		if($current_language == $userCountry){
 			return;
 		}
@@ -49,10 +73,11 @@ function getLangCodeByCountry($countryCode){
 	$path = MIX_WPML_PLUGIN_PATH.'/locale/locale.txt';
 	$file = file_get_contents($path);
 	$arr = explode("\n", $file);
-	$output = array();
 	foreach ($arr as $loc){
 		$s = explode('-', $loc);
+
 		$country = end($s);
+
 		if($country == $countryCode){
 			$lang = $s[0];
 			return $lang;
@@ -62,15 +87,54 @@ function getLangCodeByCountry($countryCode){
 	return false;
 }
 
-function MIXGetCountryByUserIp(){
-	$databases = MIX_WPML_PLUGIN_PATH.'/IP2Location-PHP-Module-master/databases/IPV6-COUNTRY-SAMPLE.BIN';
-	
-	$db = new \IP2Location\Database($databases, \IP2Location\Database::FILE_IO);
-	$records = $db->lookup($_SERVER['REMOTE_ADDR'], \IP2Location\Database::ALL);
-	return $records;
+function getCountryByLangCode($lang){
+	$path = MIX_WPML_PLUGIN_PATH.'/locale/locale.txt';
+	$file = file_get_contents($path);
+	$arr = explode("\n", $file);
+	$output = array();
+	foreach ($arr as $loc){
+		$s = explode('-', $loc);
+
+		$slang = $s[0];
+
+		if($slang != $lang){
+			continue;
+		}
+		foreach($s as $ccode){
+			if($ccode ==$lang){
+				continue;
+			}
+			$output[] = $ccode;
+		}
+	}
+	return $output;
 }
 
+function MIXGetCountryByUserIp(){
+	$query = unserialize(file_get_contents('http://ip-api.com/php/'.$_SERVER['REMOTE_ADDR']));
+	return $query;
+}
 
+function MIXGetCountryByIp($ip){
+	$query = unserialize(file_get_contents('http://ip-api.com/php/'.$ip));
+	return $query;
+}
 
+function forbidenCountry($countryCode){
+	$userCountry = MIXGetCountryByUserIp();
+	
+	if($countryCode !== $userCountry['countryCode']){
+		return;
+	}
+	global $sitepress;
+	$current_language = $sitepress->get_current_language();
+	if($current_language != 'he'){
+		return;
+	}
+	$redirect = $sitepress->language_url( 'en' );
+	wp_redirect($redirect);
+	die();
+
+}
 
 ?>

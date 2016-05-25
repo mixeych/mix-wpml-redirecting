67ae9f0cc11b1c369863a8b9da360bec05bf3c15
mixeych <dmitriy_miheev@mail.ru>
2016-05-25 17:34:31 +0300

    change db

---
+++ locale/flags.php
@@ -0,0 +1,18 @@
+<?php
+
+return array(
+	'CA' => 'canada.png',
+	'UK' => 'uk_.png',
+	'BR' => 'br.png',
+	'GT' => 'gt.png',
+	'MX' => 'mx.png',
+	'EC' => 'ec.png',
+	'CL' => 'cl.png',
+	'PA' => 'pa.png',
+	'UY' => 'uy.png',
+	'CR' => 'cr.png',
+	'PE' => 'pe.png',
+	'CO' => 'co.png',
+	'AU' => 'au.png',
+	'US' => 'us.png',
+);
